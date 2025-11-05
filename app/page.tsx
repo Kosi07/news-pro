9817084b422854f92f248c4e6a0f65f5f046928e
Kosi07 <kosiossai07@gmail.com>
2025-11-05 12:00:59 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Navbar from '@/components/Navbar'
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>Main</main>
+
+      <footer>Footer</footer>
+    </>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-row justify-between'>
+        Hamburger
+        <h1 className='text-3xl'>News Pro</h1>
+        User profile
+    </nav>
+  )
+}
+
+export default Navbar
